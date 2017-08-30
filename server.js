25896c17eb3f0bb103fd2df111fcab197fc39f99
jensjoh01 <johnnyj2439@gmail.com>
2017-08-30 15:13:59 -0700

    finished lab

--- server.js
+++ server.js
@@ -14,20 +14,6 @@ const conString = process.env.DATABASE_URL || 'postgres://localhost:5432'; // DO
 const client = new pg.Client(conString);
 client.connect();
 
-//___________________ heroku postgres
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
-// //____________________________heroku postgres
-
 client.on('error', err => console.error(err));
 
 app.use(bodyParser.json());
